08238ecc59daf34c2652c1254a9dd83c11dd4676
shivaram <deepakreddy200527@gmail.com>
2024-12-31 14:29:35 +0530

    Sample Maven Java

---
+++ src/main/java/SElabexam/samplemavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package SElabexam.samplemavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
